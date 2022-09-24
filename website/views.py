b085a81a9367e91210dd0d2068bb6a5667b48afe
Sreeja Bhattacharya <sreejabhatta8@gmail.com>
2022-09-24 14:27:50 +0530

    Adding all files

---
+++ website/views.py
@@ -0,0 +1,47 @@
+from unicodedata import category
+from unittest import result
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user #to access all info about the currently logged in user
+from .models import Note
+from . import db
+import json
+
+views = Blueprint('views', __name__)
+
+@views.route('/', methods=['GET', 'POST'])
+def home():
+    notes = Note.query.all()
+
+    # for note in notes:
+    #     #print (note.data)
+    return render_template("home.html", result=notes, user=current_user)
+
+
+@views.route('/editor_news', methods=['GET', 'POST']) #decorator
+@login_required
+def editor_news():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template("editor_news.html", user=current_user)
+
+
+@views.route('/delete-note', methods=['POST'])
+def delete_route():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            
+    return jsonify({})
